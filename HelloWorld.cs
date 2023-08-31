0fcfddb90c10a54ea4e9eb13baeea3354aabf84d
javakhiryu <javakhiryulchibaev@gmail.com>
2023-08-31 17:56:16 -0400

    Hello World file

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class int main(string[] args)
+    {
+        HelloWorld();
+    }
+    public void HelloWorld()
+    {
+        Console.WriteLine("Hello world!.");
+    }
+}
